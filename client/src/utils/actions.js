ee7aabe73be7976a28c54cb158152629d1b98141
sidonie <shelliednnguyen@gmail.com>
2021-05-12 11:15:26 -0700

    cart tests

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -14,3 +14,10 @@ previous two actions we created, in that we want to be able to select a category
 from the state created by the UPDATE_CATEGORIES action and display products for
 that category from the list we create from the UPDATE_PRODUCTS action. */
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
